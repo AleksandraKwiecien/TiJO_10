180805dc709da8cc2d04359e2718dca633d487e8
Kwiecien Aleksadnra <blackshaira@gmail.com>
2016-05-11 14:23:03 +0200

    fixed

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -23,31 +23,31 @@ describe('app', function () {
 
 
         it('return false date STRING', function () {
-            expect(app.assignToSwimmingCourse('Jack', 'error')).toEqual({
-                name: 'Jack', age: 20, course: 'adults'
+            expect(app.assignToSwimmingCourse('Jack', '12/12/2010')).toEqual({
+                name: 'Jack', age: 5, course: 'kids'
             });
         });
         it('return age 20', function () {
             expect(app.assignToSwimmingCourse('Jack', '10/06/2010')).toEqual({
-                name: 'Jack', age: 20, course: 'teens'
+                name: 'Jack', age: 5, course: 'kids'
             });
         });
         it('return adults', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'Mario', age: 14, course: 'adults'
+                name: 'John', age: 14, course: 'teens'
             });
         });
     });
 
     describe('calculateArea', function () {
         it('a, b, c are proper', function () {
-          expect(answer.calculateArea(15, 10, 0, 'good', 'bad')).toEqual({ area : 11, message : 'bad' });
+          expect(app.calculateArea(15, 10, 0, 'good', 'bad')).toEqual({ area : 15, message : 'good' });
         });
         it('insert 0 as first and last value', function () {
-           expect(answer.calculateArea(0, 10, 0, 'good', 'bad')).toEqual({ area : 10, message : 'good' });
+           expect(app.calculateArea(0, 10, 0, 'good', 'bad')).toEqual({ area : 10, message : 'good' });
         });
         it('return good', function () {
-           expect(answer.calculateArea(15, 10, 5, 'good', 'bad')).toEqual({ area : 20, message : 'good' });
+           expect(app.calculateArea(-40, 10, 5, 'good', 'bad')).toEqual({ area : 210, message : 'good' });
         });
     });    
 
